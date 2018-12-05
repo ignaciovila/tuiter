79d31ceb46d660e51746b5788e7e85f879d4127f
Ignacio Vila <ignacio.vilagraca@mercadolibre.com.ar>
2018-12-05 11:41:26 -0300

    add first test

---
+++ src/service/tweet_manager.go
@@ -0,0 +1,9 @@
+package service
+
+// Tweet current value of tweet
+var Tweet string
+
+// PublishTweet assign tweet to Tweet
+func PublishTweet(tweet string) {
+	Tweet = tweet
+}
---
+++ src/service/tweet_manager_test.go
@@ -0,0 +1,17 @@
+package service_test
+
+import (
+	"testing"
+
+	"github.com/ignaciovila/tuiter/src/service"
+)
+
+func TestPublishedTweetIsSaved(t *testing.T) {
+	var tweet string = "This is my first tweet"
+
+	service.PublishTweet(tweet)
+
+	if service.Tweet != tweet {
+		t.Error("Expected tweet is", tweet)
+	}
+}
